ea76f917f37d35f49324861919ecf2c0923af1b9
Shashank Rathor <shashankrathor180@gmail.com>
2020-08-24 23:36:24 +0530

    Updates

---
+++ precache-manifest.e1ad5f53c71545ad0d8737375dbe3cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c2049b088df87ef726b3751e25f22c",
+    "url": "/Corona_tracker/index.html"
+  },
+  {
+    "revision": "3ddea04639a11fff5088",
+    "url": "/Corona_tracker/static/css/main.c91f86e5.chunk.css"
+  },
+  {
+    "revision": "d30c0a29df6199a1141c",
+    "url": "/Corona_tracker/static/js/2.8b178eed.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Corona_tracker/static/js/2.8b178eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddea04639a11fff5088",
+    "url": "/Corona_tracker/static/js/main.6514f109.chunk.js"
+  },
+  {
+    "revision": "1022a4a3dab7bdf643a3",
+    "url": "/Corona_tracker/static/js/runtime-main.94b79614.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona_tracker/static/media/image.d7265326.png"
+  }
+]);
